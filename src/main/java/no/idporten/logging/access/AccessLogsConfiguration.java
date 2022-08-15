4e9807e96ac79d062f1b180209d4ff21e6d25437
Randi Øyri <randi.oyri@digdir.no>
2022-08-15 10:01:03 +0200

    MP-138: Set content-length in response with ServletFilter (#9)
    * MP-138: Tests of responsefilter/refactoring

--- src/main/java/no/idporten/logging/access/AccessLogsConfiguration.java
+++ src/main/java/no/idporten/logging/access/AccessLogsConfiguration.java
@@ -4,8 +4,11 @@ import ch.qos.logback.access.tomcat.LogbackValve;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class AccessLogsConfiguration {
@@ -19,6 +22,16 @@ public class AccessLogsConfiguration {
             logbackValve.setAsyncSupported(true);
             factory.addContextValves(logbackValve);
         };
+    }
+
+    @Bean
+    public FilterRegistrationBean<ResponseFilter> filterRegistrationBean() {
+        FilterRegistrationBean<ResponseFilter> filterBean = new FilterRegistrationBean<>();
+        filterBean.setFilter(new ResponseFilter());
+        filterBean.setUrlPatterns(List.of("*"));
+        filterBean.setOrder(Integer.MAX_VALUE); // Want this filter last of filters in case the other filters do something with the response
+        LoggerFactory.getLogger(AccessLogsConfiguration.class).info(ResponseFilter.class.getName() + " order: " + filterBean.getOrder());
+        return filterBean;
     }
 }
 
