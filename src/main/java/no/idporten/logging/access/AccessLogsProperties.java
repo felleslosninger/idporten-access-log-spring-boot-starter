08253af58ff0fd2c353a5ca512263c7134aa698e
Sigvald Høyheim <114908757+shoyheim-dd@users.noreply.github.com>
2023-01-06 13:56:56 +0100

    ID-2348: Forbetre lasting av bønner (#23)
    Forbetre lasting av bønner

--- src/main/java/no/idporten/logging/access/AccessLogsProperties.java
+++ src/main/java/no/idporten/logging/access/AccessLogsProperties.java
@@ -1,15 +1,11 @@
 package no.idporten.logging.access;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 /**
  * Config for ID-porten-specific cookies.
  */
-@Configuration
-@Component
 @ConfigurationProperties(ignoreInvalidFields = true, prefix = "spring.application")
 @Validated
 public class AccessLogsProperties {
