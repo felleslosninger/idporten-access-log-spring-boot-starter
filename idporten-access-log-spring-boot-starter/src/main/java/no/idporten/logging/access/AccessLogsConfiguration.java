2f191b038ec4835191e75e1e0b67347c742aeff5
Kim Taavo <kim.taavo@digdir.no>
2023-05-24 10:49:53 +0200

    removed ResponseFilter

--- idporten-access-log-spring-boot-starter/src/main/java/no/idporten/logging/access/AccessLogsConfiguration.java
+++ idporten-access-log-spring-boot-starter/src/main/java/no/idporten/logging/access/AccessLogsConfiguration.java
@@ -5,11 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 @EnableConfigurationProperties(AccessLogsProperties.class)
@@ -36,14 +34,5 @@ public class AccessLogsConfiguration {
         };
     }
 
-//    @Bean
-//    public FilterRegistrationBean<ResponseFilter> filterRegistrationBean() {
-//        FilterRegistrationBean<ResponseFilter> filterBean = new FilterRegistrationBean<>();
-//        filterBean.setFilter(new ResponseFilter());
-//        filterBean.setUrlPatterns(List.of("*"));
-//        filterBean.setOrder(Integer.MAX_VALUE); // Want this filter last of filters in case the other filters do something with the response
-//        LoggerFactory.getLogger(AccessLogsConfiguration.class).info(ResponseFilter.class.getName() + " order: " + filterBean.getOrder());
-//        return filterBean;
-//    }
 }
 
