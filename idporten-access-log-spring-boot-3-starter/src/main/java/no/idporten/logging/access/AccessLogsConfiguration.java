ccceb3c8db945117060c2582a5b3ed07ee9b54b6
Randi Øyri <randi.oyri@digdir.no>
2023-10-12 13:00:26 +0200

    ID-3404: Overstyre logback-access.xml frå applikasjon (#69)

--- idporten-access-log-spring-boot-3-starter/src/main/java/no/idporten/logging/access/AccessLogsConfiguration.java
+++ idporten-access-log-spring-boot-3-starter/src/main/java/no/idporten/logging/access/AccessLogsConfiguration.java
@@ -2,6 +2,7 @@ package no.idporten.logging.access;
 
 import no.idporten.logging.access.tomcat.LogbackValve;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -20,16 +21,19 @@ public class AccessLogsConfiguration {
         return properties;
     }
 
+    @Value("${digdir.access.logging.config-file:logback-access.xml}")
+    String logConfigfile;
+
     @Bean
     @ConditionalOnProperty(prefix = "tomcat", name = "accesslog", havingValue = "enabled", matchIfMissing = true)
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> accessLogsCustomizer(AccessLogsProperties props) {
         LoggerFactory.getLogger(AccessLogsConfiguration.class).info("Initialize accessLogsCustomizer for Tomcat Access Logging as JSON" ); 
         if(properties == null){
             properties = props;
-        } 
+        }
         return factory -> {
             var logbackValve = new LogbackValve();
-            logbackValve.setFilename("logback-access.xml");
+            logbackValve.setFilename(logConfigfile);
             logbackValve.setAsyncSupported(true);
             factory.addContextValves(logbackValve);
         };
