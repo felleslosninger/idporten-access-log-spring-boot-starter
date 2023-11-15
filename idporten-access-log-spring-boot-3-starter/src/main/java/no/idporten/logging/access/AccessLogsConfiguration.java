b0c252c15b2ae2c4656731e08de4ca73e832eb5e
Randi Øyri <randi.oyri@digdir.no>
2023-11-15 14:54:10 +0100

    ID-3537: Featureswitch for debug-logging

--- idporten-access-log-spring-boot-3-starter/src/main/java/no/idporten/logging/access/AccessLogsConfiguration.java
+++ idporten-access-log-spring-boot-3-starter/src/main/java/no/idporten/logging/access/AccessLogsConfiguration.java
@@ -13,30 +13,61 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(AccessLogsProperties.class)
 public class AccessLogsConfiguration {
-    
+
+
     // Static properties to make spring application properties available to AccesslogProvider
     private static AccessLogsProperties properties = null;
 
-    public static AccessLogsProperties getProperties(){
+    protected static final String DEFAULT_LOGBACK_CONFIG_FILE = "logback-access.xml";
+    protected static final String LOGBACK_CONFIG_REQ_FULL_FILE = "logback-access-req-full.xml";
+    protected static final String LOGBACK_CONFIG_REQ_RESP_FULL_FILE = "logback-access-req-resp-full.xml";
+
+    public static AccessLogsProperties getProperties() {
         return properties;
     }
 
     @Value("${digdir.access.logging.config-file:logback-access.xml}")
     String logConfigfile;
 
+    @Value("${digdir.access.logging.debug-level:}")
+    String debugLevel;
+
+
     @Bean
     @ConditionalOnProperty(prefix = "tomcat", name = "accesslog", havingValue = "enabled", matchIfMissing = true)
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> accessLogsCustomizer(AccessLogsProperties props) {
-        LoggerFactory.getLogger(AccessLogsConfiguration.class).info("Initialize accessLogsCustomizer for Tomcat Access Logging as JSON" ); 
-        if(properties == null){
+        if (properties == null) {
             properties = props;
         }
+        String logbackConfigFile = checkIfDebugFeatureEnabledAndConfigureLogbackfile(logConfigfile, debugLevel);
+
+        LoggerFactory.getLogger(AccessLogsConfiguration.class).info("Initialize accessLogsCustomizer for Tomcat Access Logging as JSON. Use config-file: " + logbackConfigFile);
+
         return factory -> {
             var logbackValve = new LogbackValve();
-            logbackValve.setFilename(logConfigfile);
+            logbackValve.setFilename(logbackConfigFile);
             logbackValve.setAsyncSupported(true);
             factory.addContextValves(logbackValve);
         };
+    }
+
+    /**
+     * Use application provided logback-access.xml if property digdir.access.logging.config-file is configured,
+     * otherwise check if debug-level is configured.
+     *
+     * @param configFile
+     * @param debug
+     */
+    protected String checkIfDebugFeatureEnabledAndConfigureLogbackfile(String configFile, String debug) {
+
+        if (DEFAULT_LOGBACK_CONFIG_FILE.equals(configFile) && debug != null && !debug.isEmpty()) {
+            if ("request".equalsIgnoreCase(debug)) {
+                return LOGBACK_CONFIG_REQ_FULL_FILE;
+            } else if ("response".equalsIgnoreCase(debug)) {
+                return LOGBACK_CONFIG_REQ_RESP_FULL_FILE;
+            }
+        }
+        return configFile;
     }
 
 }
