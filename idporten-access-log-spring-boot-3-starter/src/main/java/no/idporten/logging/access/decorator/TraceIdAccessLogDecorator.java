3fc102da866dc84d0369be6714a48e81d2344c08
Kim Taavo <kim.taavo@digdir.no>
2025-10-24 16:18:56 +0200

    wip

--- idporten-access-log-spring-boot-3-starter/src/main/java/no/idporten/logging/access/decorator/TraceIdAccessLogDecorator.java
+++ idporten-access-log-spring-boot-3-starter/src/main/java/no/idporten/logging/access/decorator/TraceIdAccessLogDecorator.java
@@ -15,27 +15,37 @@ public class TraceIdAccessLogDecorator implements AccessLogDecorator {
 
     @Override
     public void writeTo(JsonGenerator jsonGenerator, IAccessEvent iAccessEvent) throws IOException {
-        String traceparent = iAccessEvent.getRequestHeader("traceparent");
-        //parent-id also known as span-id
 
         // Fetch from request header first, otherwise from spancontext
-        if (traceparent != null && traceparent.contains("-") && traceparent.split("-").length == 4) {
-            String[] split = traceparent.split("-");
-            jsonGenerator.writeStringField(AccessLogFields.TRACE_ID, split[1]);
-            jsonGenerator.writeStringField(AccessLogFields.SPAN_ID, split[2]);
-        } else if (Span.current() != null && Span.current().getSpanContext() != null && Span.current().getSpanContext().getTraceId() != null) {
+        if (writeFields(jsonGenerator, iAccessEvent.getRequestHeader("traceparent"))) {
+            return;
+        } else if (Span.current().getSpanContext() != null && Span.current().getSpanContext().getTraceId() != null) {
             log.debug("traceparent not found as request header: {}", iAccessEvent);
             SpanContext spanContext = Span.current().getSpanContext();
             String traceId = spanContext.getTraceId();
-            if (traceId == null || traceId.isBlank()) {
-                log.debug("traceId not found in current span: {}", spanContext);
-                return;
-            }
             String spanId = spanContext.getSpanId();
+
+            if (traceId == null || traceId.isBlank() || traceId.equals("00000000000000000000000000000000")) { // invalid span context - check request attributes for manually added traceparent attribute
+                log.debug("Invalid span: {}. Will look for manually added traceparent in request attributes.", spanContext);
+                if(writeFields(jsonGenerator, iAccessEvent.getAttribute("traceparent"))) {
+                    return;
+                }
+            }
+
             jsonGenerator.writeStringField(AccessLogFields.TRACE_ID, traceId);
             jsonGenerator.writeStringField(AccessLogFields.SPAN_ID, spanId);
         } else {
             log.debug("traceparent not found as request header: {} or in spanContext: {}", iAccessEvent, Span.current().getSpanContext());
         }
+    }
+
+    private static boolean writeFields(JsonGenerator jsonGenerator, String traceparent) throws IOException {
+        if (traceparent != null && traceparent.contains("-") && traceparent.split("-").length == 4) {
+            String[] split = traceparent.split("-");
+            jsonGenerator.writeStringField(AccessLogFields.TRACE_ID, split[1]);
+            jsonGenerator.writeStringField(AccessLogFields.SPAN_ID, split[2]);
+            return true;
+        }
+        return false;
     }
 }
