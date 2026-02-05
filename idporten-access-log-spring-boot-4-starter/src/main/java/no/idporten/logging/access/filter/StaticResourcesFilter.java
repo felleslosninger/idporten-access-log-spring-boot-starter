40c556770ba72353a6fac35fb000266c152d831b
Nils-Jarle Haugen <nils-jarle.haugen@digdir.no>
2026-02-05 13:37:54 +0100

    fix: only apply filtering on successful responses (like old imp)

--- idporten-access-log-spring-boot-4-starter/src/main/java/no/idporten/logging/access/filter/StaticResourcesFilter.java
+++ idporten-access-log-spring-boot-4-starter/src/main/java/no/idporten/logging/access/filter/StaticResourcesFilter.java
@@ -4,6 +4,7 @@ import ch.qos.logback.access.common.spi.IAccessEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerMapping;
@@ -15,9 +16,7 @@ public class StaticResourcesFilter extends Filter<IAccessEvent> {
 
     private static StaticResourcesFilterProperties properties;
 
-    /**
-     * Called by Spring via {@link StaticResourcesFilterConfiguration} to inject properties.
-     */
+    //fixme: @injection fungerte ikkje, brute-force her fungerer, men fortsatt ikkje heilt bra..
     public static void setProperties(StaticResourcesFilterProperties props) {
         properties = props;
         LOG.debug("Configured with properties: paths={}, staticResources={}",
@@ -36,21 +35,26 @@ public class StaticResourcesFilter extends Filter<IAccessEvent> {
         final var filterStaticResources = properties.staticResources();
 
         final HttpServletRequest request = accessEvent.getRequest();
+        final HttpServletResponse response = accessEvent.getResponse();
 
-        if (filterStaticResources) {
-            // handle application static resources
-           var handlerObject = request.getAttribute(HandlerMapping.BEST_MATCHING_HANDLER_ATTRIBUTE);
-           if (handlerObject instanceof ResourceHttpRequestHandler) {
-               return FilterReply.DENY;
-           }
-        }
 
-        if (filterPaths != null) {
-            // handle custom paths
-            String requestUri = request.getRequestURI();
-            for (var filterPath : filterPaths) {
-                if (requestUri.startsWith(filterPath)) {
+        // consider filtering logs on successful responses
+        if (response.getStatus() < 400) {
+            if (filterStaticResources) {
+                // handle application static resources
+                var handlerObject = request.getAttribute(HandlerMapping.BEST_MATCHING_HANDLER_ATTRIBUTE);
+                if (handlerObject instanceof ResourceHttpRequestHandler) {
                     return FilterReply.DENY;
+                }
+            }
+
+            if (filterPaths != null) {
+                // handle custom paths
+                String requestUri = request.getRequestURI();
+                for (var filterPath : filterPaths) {
+                    if (requestUri.startsWith(filterPath)) {
+                        return FilterReply.DENY;
+                    }
                 }
             }
         }
